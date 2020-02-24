8320cbc3a709329e23aec0441292e85a1b040c1c
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-24 14:14:22 +0530

    Removed console.log statement in getVowelsCount function

--- countVowels.js
+++ countVowels.js
@@ -2,14 +2,13 @@ function getVowelsCount(word)
 {
     let vowelsRegEx = /[aeiouAEIOU]/g;
     let vowelsInWord = word.match(vowelsRegEx);
-    console.log(vowelsInWord)
     let vowelsCount = vowelsInWord.length;
     return vowelsCount;
 }
 
 function printVowelCount(word, count)
 {
-    console.log("Number  of vowels in " + word +" is : " + count);
+    console.log("Number of vowels in " + word +" is : " + count);
 }
 
 function main()
