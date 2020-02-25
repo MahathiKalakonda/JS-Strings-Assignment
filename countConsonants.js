6e5c8bea5c8bb66551e138479bf71bcca8f44fcb
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-25 11:28:43 +0530

    removed print function

--- countConsonants.js
+++ countConsonants.js
@@ -1,19 +1,14 @@
-function getMatchedCount(regularExpression, word)
+function getMatchedCount(pattern, word)
 {
-    let matchedWords = word.match(regularExpression);
-    let vowelsCount = matchedWords.length;
-    return vowelsCount;
-}
-
-function printConsonantCount(word, count)
-{
-    console.log("Number of consonants in " + word +" is : " + count);
+    let matchedWords = word.match(pattern);
+    let matchedCount = matchedWords.length;
+    return matchedCount;
 }
 
 function main()
 {
-    let word = "Water BoTtle", consonantsRegExp = /[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/g;
-    console.log("Number of consonants in " + word +" is : " + getMatchedCount(consonantsRegExp, word));
+    let word = "Water BoTtle", consonantsPattern = /[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/g;
+    console.log("Number of consonants in " + word +" is : " + getMatchedCount(consonantsPattern, word));
 }
 
 main()
