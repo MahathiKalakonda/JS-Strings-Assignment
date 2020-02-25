3098f345d88be87860df9e711447bc5bca3c2219
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-25 12:03:53 +0530

    created strip.js file

---
+++ strip.js
@@ -0,0 +1,12 @@
+function getStripedSentence(sentence)
+{
+
+}
+
+function main()
+{
+    let sentence = '    Mahathi Kalakonda     '
+    console.log("Striped sentence of " + sentence + " is : " + getStripedSentence(sentence));
+}
+
+main();
