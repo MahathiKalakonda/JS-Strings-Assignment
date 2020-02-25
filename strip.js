75d1695251d842a58f075b88edfd05aceee32da6
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-25 12:34:06 +0530

    Added stripAtEnd functionality

--- strip.js
+++ strip.js
@@ -1,6 +1,21 @@
+function stripAtEnd(words)
+{
+    let index = words.length - 1;
+    while(words[index] == '')
+    {
+        words.pop();
+        index--;
+    }
+    return words;
+}
+
 function getStripedSentence(sentence)
 {
-
+    let words = sentence.split(" ");
+    words = stripAtEnd(words);
+    words = stripAtEnd(words.reverse());
+    words = words.reverse();
+    return words.join(" ");
 }
 
 function main()
