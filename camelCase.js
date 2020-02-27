6b77ebf8c7e00fafa8969b31b48581e74db1e97c
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-27 16:08:25 +0530

    used reduce function

--- camelCase.js
+++ camelCase.js
@@ -1,23 +1,19 @@
 function getTitleCase(word)
 {
-    word = word.toLowerCase();
-    return word[0].toUpperCase()+word.slice(1);
+    return word[0].toUpperCase()+word.slice(1).toLowerCase();
 }
 
 function getCamelCase(words)
 {
-    camelCase = words[0].toLowerCase();
-    for(let index = 1; index < words.length; index++)
-    {
-        let titleCaseWord = getTitleCase(words[index]);
-        camelCase = camelCase.concat(titleCaseWord);
-    }
-    return camelCase;
+    return words.slice(1).reduce((accumulator,word) =>
+                            accumulator.concat(getTitleCase(word)),
+                            words[0].toLowerCase());
 }
 
 function main()
 {
-    console.log("Camel case word is : " + getCamelCase(["this", "is", "mY", "sandWich"]));
+    console.log(getTitleCase("mahati"));
+    console.log("Camel case word is : " + getCamelCase(["This", "iS", "mY", "sandWich"]));
 }
 
 main();
