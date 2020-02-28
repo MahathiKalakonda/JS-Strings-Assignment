dcc7bd6c0043455213f944e3d12b42e334ec8958
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-28 10:22:51 +0530

    used reduce function

--- interleave.js
+++ interleave.js
@@ -1,11 +1,9 @@
 function insertDelimiter(text, delimiter)
 {
-    let resultText = text[0];
-    for(let index = 1; index < text.length; index++)
-    {
-        resultText=resultText + delimiter + text[index];
-    }
-    return resultText;
+    return text.split("").reduce(function(resultText, element)
+                        {
+                            return resultText.concat(delimiter,element);
+                        });
 }
 
 function main()
